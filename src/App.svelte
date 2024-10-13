6eb495266670808cd699d0c4c86c415b384a2c2e
rob00x <rob00x@gmail.com>
2024-10-13 22:18:46 +0000

    Add template from idx

---
+++ src/App.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+  import { MapLibre } from 'svelte-maplibre';
+</script>
+
+<main>
+  <div>
+    <h1>WldMap</h1>
+    <MapLibre 
+    center={[-1,53]}
+    zoom={2}
+    class="map"
+    standardControls
+    style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json" />
+  </div>
+
+
+  <div class="card">
+    <Counter />
+  </div>
+
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+  :global(.map) {
+    width: 500px;
+    height: 500px;
+  }
+</style>
